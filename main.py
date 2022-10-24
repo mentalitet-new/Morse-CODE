2a9d4fb742b32a30927b1c6914afa94ef6887102
mentalitet <pro@system-sec.ru>
2022-10-24 14:20:38 +0300

    add: add decode function and key.json

--- main.py
+++ main.py
@@ -1,58 +1,58 @@
-import psycopg2
 import json
 
 
-class PasswordsGenerator:
+class MorseCode:
     def __init__(self):
-        super(PasswordsGenerator, self).__init__()
+        super(MorseCode, self).__init__()
+        self.__all_symbol_encode = []
         self.__cur = None
         self.__conn = None
         self.__total_symbol = []
 
-    def __open_json(self):
-        with open("data.json", "r") as read:
+    def __open_json_data(self):
+        with open("connect.json", "r") as read:
             data = json.load(read)
         return data
 
-    def start_connect(self):
-        self.__conn = psycopg2.connect(f'dbname={self.__open_json()["dbname"]} user={self.__open_json()["user"]} '
-                                       f'password={self.__open_json()["password"]} '
-                                       f'host={self.__open_json()["host"]} '
-                                       f'port={self.__open_json()["port"]} ')
-        self.__cur = self.__conn.cursor()
-
-    def fetch_all(self):
-        self.__cur.execute("SELECT * FROM information")
-        rec = self.__cur.fetchall()
-        return rec
-
-    def encode_algorithm(self, symbol):
-        all_symbol = []
+    def __open_json_key(self):
         with open("key.json", "r") as read:
             data = json.load(read)
+        return data
 
-        if symbol is not None:
-            self.__total_symbol.append(symbol)
+    def encode_algorithm(self, symbol_encode):
+        data = self.__open_json_key()
+        if symbol_encode is not None:
+            self.__total_symbol.append(symbol_encode)
         else:
             pass
 
-        for i in self.__total_symbol[0]:
+        for i in self.__total_symbol[0].upper():
             k = i
             if k in data["alphabet"]:
-                all_symbol.append(data["alphabet"][k])
+                self.__all_symbol_encode.append(data["alphabet"][k])
             elif k in data["numbers"]:
-                all_symbol.append(data["numbers"][k])
-        return all_symbol
+                self.__all_symbol_encode.append(data["numbers"][k])
+        return self.__all_symbol_encode
 
-    def decode_algorithm(self):
-        pass
+    def decode_algorithm(self, symbol_decode):
+        data = self.__open_json_key()
+        decode_list_password = symbol_decode
+        if decode_list_password is not None:
+            for i in range(len(decode_list_password)):
+                for key, value in data["alphabet"].items():
+                    if value == decode_list_password[i]:
+                        decode_list_password[i] = key
+                for key, value in data["numbers"].items():
+                    if value == decode_list_password[i]:
+                        decode_list_password[i] = key
+            return decode_list_password
+        else:
+            return None
 
 
 if __name__ == '__main__':
-    class_data = PasswordsGenerator()
-    class_data.start_connect()
-    class_data.fetch_all()
-    print(class_data.encode_algorithm("1AD12"))
-
-    print(class_data.encode_algorithm("1AD12"))
+    class_data = MorseCode()
+    symbol = class_data.encode_algorithm("SOS123")
+    print(symbol)
+    print(class_data.decode_algorithm(symbol))
 
